6405b323be94a1ea040358a08de20ec38f2fc24e
santtiiojeda8 <ojedasi2004@gmail.com>
2025-08-25 21:23:14 -0300

    3 Secciones terminadas

---
+++ Platilla-Agente/astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import path from 'path';
+
+// https://astro.build/config
+export default defineConfig({
+    "devToolbar" : {
+        enabled : false
+    }
+});
